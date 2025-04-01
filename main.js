e28cdf990693fafa447123523c9de9135806d9e9
shaiksafroz12345 <shaikafroz2k4@gmail.com>
2025-04-01 16:02:55 +0530

    updated

---
+++ main.js
@@ -0,0 +1,10 @@
+import {add,subtract,greet} from "./calc.js"
+let result1=add(2,3)
+let result2=subtract(7,5)
+let str=greet()
+console.log(result1)
+console.log(result2)
+console.log(str);
+// import {add} from "./calc.js"
+// let result=add(2,3)
+// console.log(result)
